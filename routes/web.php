13f4d993d944e333067ab2ab3b1b05231935a044
SEIYA <skkasans@gmail.com>
2020-07-03 00:03:58 +0900

    ユーザー登録機能の作成
    ・ユーザー登録画面、ログイン画面の作成
    ・認証機能の実装
    ・バリデーションの日本語化

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
 Route::get('/', 'ArticleController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
